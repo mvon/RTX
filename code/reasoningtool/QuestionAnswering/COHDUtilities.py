b4c88e5b3318d72f0b809f6c2c022a699462d41f
Finn Womack <flan313@gmail.com>
2018-09-20 08:28:00 -0700

    #284, added get_surprising_conditions_treating method

--- code/reasoningtool/QuestionAnswering/COHDUtilities.py
+++ code/reasoningtool/QuestionAnswering/COHDUtilities.py
@@ -80,6 +80,60 @@ class COHDUtilities:
 
 		return result_dict
 
+	@staticmethod
+	def get_surprising_conditions_treating(drug_description, conservative=False):
+		"""
+		Get all the suprising conditions that are associated with a drug.
+		:param drug_description: string (eg. 'Naproxen')
+		:param conservative: bool (True= use exact matching for mapping drug to COHD, False = use all synonyms returned by COHD)
+		:return: dictionary of dictionaries (eg. keys are concept IDs, values look like:
+		{'concept_2_id': 134736,
+		  'associated_2_name': 'Backache',
+		'observed_count': 200,
+		'expected_count': 100,
+		'concept_': 0.69314718056,
+		'concept_1_id': 1115008}
+		"""
+
+		# Get the concept ID of the drug
+		drug_concepts = QueryCOHD.find_concept_ids(drug_description, dataset_id=3)
+		drug_ids = []
+		if conservative:
+			for concept in drug_concepts:
+				if concept['concept_name'].lower() == drug_description.lower():
+					drug_ids.append(concept['concept_id'])
+		if not conservative:
+			for concept in drug_concepts:
+				drug_ids.append(concept['concept_id'])
+
+		# get all the associated conditions
+		associated_concepts = []
+		for drug_id in drug_ids:
+			associated_concepts += QueryCOHD.get_obs_exp_ratio(str(drug_id), domain="Condition", dataset_id=3)
+
+
+		# go through and sum them all up (no need for conservative flag since that will only be a single one)
+		# get all the unique condition ids
+		associated_concept_ids = set()
+		for concept in associated_concepts:
+			associated_concept_ids.add(concept['concept_id_2'])
+
+		# go through the associated conditions, summing up the observed counts and expected counts
+		result_dict = dict()
+		for associated_concept in associated_concepts:
+			id = associated_concept['concept_id_2']
+			if id in result_dict:
+				result_dict[id]['observed_count'] += associated_concept['observed_count']
+				result_dict[id]['expected_count'] += associated_concept['expected_count']
+			else:
+				result_dict[id] = associated_concept
+
+		# We'll need to adjust the ln_ratio to account for multiple mappings
+		for id in result_dict:
+			result_dict[id]['ln_ratio'] = math.log(result_dict[id]['observed_count'] / result_dict[id]['expected_count'])
+
+		return result_dict
+
 if __name__ == "__main__":
 	q = COHDUtilities()
 	print(q.get_conditions_treating('Naproxen', conservative=True))
